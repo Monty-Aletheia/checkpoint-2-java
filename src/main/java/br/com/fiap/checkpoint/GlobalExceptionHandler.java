0caf49c2b9599b1555917f78c94f688b386a02b9
Pedro Jesus <Pedromra.04@gmail.com>
2025-04-21 14:43:56 -0300

    add prometheus and grafana

--- src/main/java/br/com/fiap/checkpoint/GlobalExceptionHandler.java
+++ src/main/java/br/com/fiap/checkpoint/GlobalExceptionHandler.java
@@ -1,22 +1,22 @@
-package br.com.fiap.checkpoint;
-
-import jakarta.validation.UnexpectedTypeException;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-@ControllerAdvice
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler(Exception.class)
-    public String handleException(Exception ex, Model model){
-        model.addAttribute("ex", ex);
-        return "error";
-    }
-
-    @ExceptionHandler(UnexpectedTypeException.class)
-    public String handleUnexpectedTypeException(UnexpectedTypeException ex, Model model) {
-        model.addAttribute("errorMessage", ex.getMessage());
-        return "error";
-    }
-}
+//package br.com.fiap.checkpoint;
+//
+//import jakarta.validation.UnexpectedTypeException;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//
+//@ControllerAdvice
+//public class GlobalExceptionHandler {
+//
+//    @ExceptionHandler(Exception.class)
+//    public String handleException(Exception ex, Model model){
+//        model.addAttribute("ex", ex);
+//        return "error";
+//    }
+//
+//    @ExceptionHandler(UnexpectedTypeException.class)
+//    public String handleUnexpectedTypeException(UnexpectedTypeException ex, Model model) {
+//        model.addAttribute("errorMessage", ex.getMessage());
+//        return "error";
+//    }
+//}
--- src/main/java/br/com/fiap/checkpoint/controller/HomeController.java
+++ src/main/java/br/com/fiap/checkpoint/controller/HomeController.java
@@ -3,6 +3,7 @@ package br.com.fiap.checkpoint.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class HomeController {
 
